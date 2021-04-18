137831a8a40bc3a5f097d12a12d98429b63b2b0b
Karlatemp <karlatemp@vip.qq.com>
2021-04-18 13:36:54 +0800

    Sandbox mode classes loading

--- backend/mirai-console/src/internal/plugin/JvmPluginClassLoader.kt
+++ backend/mirai-console/src/internal/plugin/JvmPluginClassLoader.kt
@@ -32,7 +32,6 @@ internal class JvmPluginClassLoader(
         return "JvmPluginClassLoader{source=$file}"
     }
 
-    private val cache = ConcurrentHashMap<String, Class<*>>()
     internal var declaredFilter: ExportManager? = null
 
     companion object {
@@ -43,71 +42,37 @@ internal class JvmPluginClassLoader(
         }
     }
 
+    internal fun loadClassFromOtherClassLoader(name: String): Class<*>? {
+        findLoadedClass(name)?.let { return it }
+        return try {
+            super.findClass(name)
+        } catch (e: ClassNotFoundException) {
+            null
+        }
+    }
+
+    internal fun loadedClass(name: String): Class<*>? = super.findLoadedClass(name)
+
     override fun findClass(name: String): Class<*> {
         synchronized(kotlin.run {
             val lock = Any()
             loadingLock.putIfAbsent(name, lock) ?: lock
         }) {
-            return findClass(name, false) ?: throw ClassNotFoundException(name)
+            return findClass0(name) ?: throw ClassNotFoundException(name)
         }
     }
 
-    internal fun findClass(name: String, disableGlobal: Boolean): Class<*>? {
-        // First. Try direct load in cache.
-        val cachedClass = cache[name]
-        if (cachedClass != null) {
-            if (disableGlobal) {
-                val filter = declaredFilter
-                if (filter != null && !filter.isExported(name)) {
-                    throw LoadingDeniedException(name)
+    internal fun findClass0(name: String): Class<*>? {
+        try {
+            return super.findClass(name)
+        } catch (notFoundException: ClassNotFoundException) {
+            classLoaders.forEach { classLoader ->
+                if (classLoader.declaredFilter?.isExported(name) != false) {
+                    classLoader.loadClassFromOtherClassLoader(name)?.let { return it }
                 }
             }
-            return cachedClass
+            throw notFoundException
         }
-        if (disableGlobal) {
-            // ==== Process Loading Request From JvmPluginClassLoader ====
-            //
-            // If load from other classloader,
-            // means no other loaders are cached.
-            // direct load
-            return kotlin.runCatching {
-                super.findClass(name).also { cache[name] = it }
-            }.getOrElse {
-                if (it is ClassNotFoundException) null
-                else throw it
-            }?.also {
-                // This request is from other classloader,
-                // so we need to check the class is exported or not.
-                val filter = declaredFilter
-                if (filter != null && !filter.isExported(name)) {
-                    throw LoadingDeniedException(name)
-                }
-            }
-        }
-
-        // ==== Process Loading Request From JDK ClassLoading System ====
-
-        // First. scan other classLoaders's caches
-        classLoaders.forEach { otherClassloader ->
-            if (otherClassloader === this) return@forEach
-            val filter = otherClassloader.declaredFilter
-            if (otherClassloader.cache.containsKey(name)) {
-                return if (filter == null || filter.isExported(name)) {
-                    otherClassloader.cache[name]
-                } else throw LoadingDeniedException("$name was not exported by $otherClassloader")
-            }
-        }
-        classLoaders.forEach { otherClassloader ->
-            val other = kotlin.runCatching {
-                if (otherClassloader === this) super.findClass(name).also { cache[name] = it }
-                else otherClassloader.findClass(name, true)
-            }.onFailure { err ->
-                if (err is LoadingDeniedException || err !is ClassNotFoundException)
-                    throw err
-            }.getOrNull()
-            if (other != null) return other
-        }
-        throw ClassNotFoundException(name)
     }
 }
 
